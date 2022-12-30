b02d317d4b770e86475a7189e9a20602a8d2db90
mkursadulusoy <mkursadulusoy@hotmail.com.tr>
2022-12-30 21:57:44 +0100

    Add files via upload

---
+++ MKUComChecker/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace MKUComChecker
+{
+    public partial class Form1 : Form
+    {
+
+        public static List<string> ListCOM() {
+
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+                return portList;
+
+
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var i in ListCOM())
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var i in ListCOM())
+            {
+
+                listBox1.Items.Add(i);
+            }
+        }
+    }
+}
